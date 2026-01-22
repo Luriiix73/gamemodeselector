2a1dadd3615bd5a22a0428548e6fb742215e4a64
Luriiix73 <luis.kahnt@protonmail.com>
2026-01-22 17:01:41 +0100

    Spawn selectors at exact player position

--- src/main/java/org/lurix/gamemodeselector/SelectorManager.java
+++ src/main/java/org/lurix/gamemodeselector/SelectorManager.java
@@ -69,7 +69,7 @@ public final class SelectorManager implements Listener {
             return;
         }
 
-        Location baseLocation = player.getLocation().toCenterLocation();
+        Location baseLocation = player.getLocation().clone();
         ItemDisplay itemDisplay = player.getWorld().spawn(baseLocation, ItemDisplay.class, display -> {
             display.setItemStack(new ItemStack(material));
             display.setBillboard(Display.Billboard.FIXED);
