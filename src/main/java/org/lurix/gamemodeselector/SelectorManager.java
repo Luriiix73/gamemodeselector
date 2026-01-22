e3a3064678ba6f747b44966261324de90da77192
Luriiix73 <luis.kahnt@protonmail.com>
2026-01-22 17:01:49 +0100

    Allow selector interaction from 10 blocks

--- src/main/java/org/lurix/gamemodeselector/SelectorManager.java
+++ src/main/java/org/lurix/gamemodeselector/SelectorManager.java
@@ -34,7 +34,7 @@ import org.joml.Vector3f;
 
 public final class SelectorManager implements Listener {
     private static final float HOVER_SCALE_MULTIPLIER = 1.5f;
-    private static final double MAX_INTERACT_DISTANCE = 5.0;
+    private static final double MAX_INTERACT_DISTANCE = 10.0;
     private static final float ROTATION_STEP = 0.015f;
 
     private final Plugin plugin;
