e47ad6b4892c9f1b4f6040f475a0d0f040a0787d
Luriiix73 <luis.kahnt@protonmail.com>
2026-01-24 16:49:31 +0100

    Ensure default config is saved

--- src/main/java/org/lurix/gamemodeselector/GamemodeSelectorPlugin.java
+++ src/main/java/org/lurix/gamemodeselector/GamemodeSelectorPlugin.java
@@ -15,6 +15,7 @@ public final class GamemodeSelectorPlugin extends JavaPlugin implements CommandE
 
     @Override
     public void onEnable() {
+        saveDefaultConfig();
         selectorManager = new SelectorManager(this);
         getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
         getServer().getPluginManager().registerEvents(selectorManager, this);
